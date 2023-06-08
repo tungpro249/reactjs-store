9fda83d88cac682e093fd9e1f0a50e771e640551
thanh tung <tungt3922gmail.com>
2023-06-08 23:57:36 +0700

    .

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+      <>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+      </Routes>
+      </>
+  );
+}
+
+export default App;
