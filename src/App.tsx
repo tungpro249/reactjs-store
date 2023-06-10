ee29f84694c5be8f63e00b9fa4c5921c74e9c80c
thanh tung <tungt3922gmail.com>
2023-06-10 21:59:07 +0700

    fomat code

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import React from "react";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import About from "./components/About";
 import SignIn from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import "./App.css";
 
 function App() {
   return (
-      <>
+    <>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<SignIn/>} />
-          <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/account/login" element={<SignIn />} />
+        <Route path="/account/register" element={<Register />} />
       </Routes>
-      </>
+    </>
   );
 }
 
--- src/components/foodter/index.tsx
+++ src/components/foodter/index.tsx
@@ -1,8 +1,37 @@
+import { Box, Grid } from "@mui/material";
+import Copyright from "../copyRight";
 
 const Foodter = () => {
-    return <>
-        Foodter
+  return (
+    <>
+      <Box sx={{ background: "#000", padding: "40px" }}>
+        <Grid container color={"#fff"}>
+          <Grid item xs={3} md={3} sm={3} lg={3}>
+            <Box>
+              <h2>Shop thời trang TinTin</h2>
+              <address>
+                Địa chỉ: Lô 1+2, Ô quy hoạch E.2/NO7 đường Lâm Hạ phường Bồ Đề, quận Long Biên, Hà
+                Nội Chăm sóc khách hàng: 0243.9388512 Mua hàng online: 0246.2909098 Email:
+                nemcskh@stripe-vn.com
+              </address>
+            </Box>
+          </Grid>
+          <Grid item xs={3} md={3} sm={3} lg={3}>
+            Giới thiệu Triết lý kinh doanh tại NEM Fashion NEM's Blog Hệ thống showroom Liên hệ
+          </Grid>
+          <Grid item xs={3} md={3} sm={3} lg={3}>
+            Chính sách giao nhận - Vận chuyển Hướng dẫn thanh toán Tra cứu đơn hàng Hướng dẫn chọn
+            Size Quy định đổi hàng Quy định bảo hành và sửa chữa Khách hàng thân thiết
+          </Grid>
+
+          <Grid item xs={3} md={3} sm={3} lg={3}>
+            Phương thức thanh toán
+          </Grid>
+        </Grid>
+      </Box>
+      <Copyright />
     </>
-}
+  );
+};
 
 export default Foodter;
